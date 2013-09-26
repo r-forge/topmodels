460c23b93e0a9c25326af408cef2ec62ed78515d
zeileis <zeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-26 06:43:37 +0000

    merged all hxlr code (including methods) into one file again
    used requireNamespace("ordinal") to make ordinal::clm available (rather
    than through Depends)
    
    called first linear predictor "location" rather than "predictor" (to
    better conform with "scale")
    
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/topmodels@325 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- crch/R/hxlr.R
+++ crch/R/hxlr.R
@@ -1,5 +1,5 @@
-hxlr <-
-function(formula, scale=~NULL, data, weights = NULL, thresholds, ...) {
+hxlr <- function(formula, scale = ~ NULL, data, weights = NULL, thresholds, ...)
+{
   cl <- match.call()
   if(is.null(data)) data <- cbind(model.frame(formula), model.frame(scale))
   nobs <- nrow(data)
@@ -10,11 +10,13 @@ function(formula, scale=~NULL, data, weights = NULL, thresholds, ...) {
     weights <- weights[weights>0]
   }
 
+  stopifnot(requireNamespace("ordinal"))
+
   ## get environment from clm
-  env <- clm(as.character(formula), scale = scale, data = data, weights = weights, doFit=FALSE, ...)
+  env <- ordinal::clm(as.character(formula), scale = scale, data = data, weights = weights, doFit = FALSE, ...)
   
   ## thresholds can also be data.frame with several columns (predictor variables for intercept model)
-  q <- model.matrix(~thresholds)
+  q <- model.matrix(~ thresholds)
   p <- ncol(q)  # number of columns (predictor variables)
 
   ## new objective function
@@ -30,7 +32,7 @@ function(formula, scale=~NULL, data, weights = NULL, thresholds, ...) {
   }
     
   ## starting values from clm fit, threshold coefficients set to 0 and 1
-  strt <- c(0, rep(1, p-1)/(p-1), clm(formula, scale = scale, data = data, ...)$coefficients[-(1:nrow(q))])
+  strt <- c(0, rep(1, p-1)/(p-1), ordinal::clm(formula, scale = scale, data = data, ...)$coefficients[-(1:nrow(q))])
 
   ## estimation
   opt <- nlminb(start=strt, objective=nll2, envir=env)
@@ -72,7 +74,7 @@ function(formula, scale=~NULL, data, weights = NULL, thresholds, ...) {
   
   ## output
   rval <- list(
-    coefficients = list(intercept = alpha, predictor = beta, scale = delta2),
+    coefficients = list(intercept = alpha, location = beta, scale = delta2),
     coefficients2 = list(location = gamma, scale = delta),
     optim = opt,  
     loglik = - opt$objective,
@@ -90,3 +92,134 @@ function(formula, scale=~NULL, data, weights = NULL, thresholds, ...) {
   class(rval) <- "hxlr"
   return(rval)
 }
+
+logLik.hxlr <- function(object, ...)
+  structure(object$loglik, df = sum(sapply(object$coefficients, length)), class = "logLik")
+
+predict.hxlr <- function(object, newdata = object$data,
+  type = c("class", "probability", "cumprob", "location", "scale"),
+  thresholds = object$thresholds, ...)
+{
+  type <- match.arg(type)
+  
+  ## get coefficients
+  gamma <- object$coefficients2$location
+  delta <- object$coefficients2$scale
+
+  ## matrices for location and scale model from newdata
+  x <- model.matrix(delete.response(terms(object$formula)), newdata)
+  z <- model.matrix(object$scale, newdata)
+  
+  ## location and scale 
+  mu <- drop(x %*% gamma)
+  sigma <- exp(drop(z %*% delta))
+
+  ## cumulative probabilities P(y<threshold[i]|x)
+  cumprob <- NULL
+  for(i in 1:length(thresholds)) { 
+    cumprob <- cbind(cumprob, plogis((thresholds[i] - mu)/sigma))
+  }
+  
+  ## category probabilities P(threshold[i-1]<=y<threshold[i]|x)
+  prob <- t(apply(cbind(0, cumprob, 1), 1, diff))
+
+  rval <- switch(type,
+      "location" = mu,
+      "scale" = sigma,
+      "cumprob" = cumprob,
+      "probability" = prob,
+      "class" = apply(prob, 1, which.max)
+    )
+
+  return(rval)
+}
+
+print.hxlr <- function(x, digits = max(3, getOption("digits") - 3), ...)
+{
+  cat("\nCall:", deparse(x$call, width.cutoff = floor(getOption("width") * 0.85)), "", sep = "\n")
+  if(!x$converged) {
+    cat("model did not converge\n")
+  } else {
+    if(length(x$coefficients$location) | length(x$coefficients$intercept)) {
+      cat(paste("Coefficients:\n", sep = ""))
+       print.default(format(c(x$coefficients$intercept, x$coefficients$location), digits = digits), print.gap = 2, quote = FALSE)
+       cat("\n")
+    } else cat("No coefficients\n\n")
+    if(length(x$coefficients$scale)) {
+      cat(paste("Coefficients (scale model with log link):\n", sep = ""))
+      print.default(format(x$coefficients$scale, digits = digits), print.gap = 2, quote = FALSE)
+      cat("\n")
+    } else cat("No coefficients (in scale model)\n\n")
+    cat("---\n")
+    cat("Converted coefficients for location and scale:\n")
+    if(length(x$coefficients2$location)) {
+      cat(paste("Coefficients (location model):\n", sep = ""))
+       print.default(format(x$coefficients2$location, digits = digits), print.gap = 2, quote = FALSE)
+       cat("\n")
+    } else cat("No coefficients (in location model)\n\n")
+    if(length(x$coefficients2$scale)) {
+      cat(paste("Coefficients (scale model with log link):\n", sep = ""))
+      print.default(format(x$coefficients2$scale, digits = digits), print.gap = 2, quote = FALSE)
+      cat("\n")
+    } else cat("No coefficients (in scale model)\n\n")
+    cat("\n")
+  }
+  invisible(x)
+}
+
+summary.hxlr <- function(object, ...)
+{
+  ## extend coefficient table
+  k <- length(object$coefficients$intercept)
+  l <- length(object$coefficients$location)
+  m <- length(object$coefficients$scale)
+  cf <- as.vector(do.call("c", object$coefficients))
+  se <- sqrt(diag(object$vcov))
+  cf <- cbind(cf, se, cf/se, 2 * pnorm(-abs(cf/se)))
+  colnames(cf) <- c("Estimate", "Std. Error", "z value", "Pr(>|z|)")
+  if(length(object$coefficients$scale)) {
+    cf <- list(
+      intercept = cf[seq.int(length.out = k), , drop = FALSE],
+      location = cf[seq.int(length.out = l) + k, , drop = FALSE],
+      scale = cf[seq.int(length.out = m) + k + l, , drop = FALSE])
+    rownames(cf$scale) <- names(object$coefficients$scale)
+  } else {
+    cf <- list(intercept = cf[seq.int(length.out = k), , drop = FALSE], location = cf[seq.int(length.out = l) + k, , drop = FALSE])
+  }
+  rownames(cf$location) <- names(object$coefficients$location)
+  rownames(cf$intercept) <- names(object$coefficients$intercept)
+  object$coefficients <- cf
+
+  ## info
+  object$info <- cbind(nobs = object$nobs, logLik = object$loglik, AIC = AIC(object), niter = object$optim$iterations)
+  rownames(object$info) = ""
+
+  ## return
+  class(object) <- "summary.hxlr"
+  object
+}
+
+print.summary.hxlr <- function(x, digits = max(3, getOption("digits") - 3), ...)
+{
+  cat("\nCall:", deparse(x$call, width.cutoff = floor(getOption("width") * 0.85)), "", sep = "\n")
+  
+  if(!x$converged) {
+    cat("model did not converge\n")
+  } else {
+    print(x$info)
+    if(NROW(x$coefficients$location) | NROW(x$coefficients$intercept)) {
+      cat(paste("\nCoefficients:\n", sep = ""))
+      printCoefmat(rbind(x$coefficients$intercept, x$coefficients$location), digits = digits, signif.legend = FALSE)
+    } 
+
+    if(NROW(x$coefficients$scale)) {
+      cat(paste("\nlog-scale coefficients:\n", sep = ""))
+      printCoefmat(x$coefficients$scale, digits = digits, signif.legend = FALSE)
+    } 
+
+    if(getOption("show.signif.stars") & any(do.call("rbind", x$coefficients)[, 4L] < 0.1))
+      cat("---\nSignif. codes: ", "0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1", "\n")
+  }
+  invisible(x)
+}
+
