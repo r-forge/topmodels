86e8a861cec6023683032bb47239864c39603d72
jakob <jakob@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-05-11 11:17:57 +0000

    removed fcnorm minor touch-ups
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/topmodels@598 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- crch/R/cnorm.R
+++ crch/R/cnorm.R
@@ -12,7 +12,6 @@ pcnorm <- function(q, mean, sd, left = -Inf, right = Inf) {
   pnorm((q-mean)/sd, log = log)))
 }
 
-
 rcnorm <- function(n, mean, sd, left = -Inf, right = Inf) {
   rval <- rnorm(n) * sd + mean
   pmax(pmin(rval, right), left)
@@ -22,9 +21,6 @@ qcnorm <- function(p, mean, sd, left = -Inf, right = Inf) {
   rval <- qnorm(p) * sd + mean
   pmax(pmin(rval, right), left)
 }
-
-
-
 
 scnorm <- function(x, mean = 0, sd = 1, which = c("mu", "sigma"), left = -Inf, right = Inf) {
   if(!is.character(which))
@@ -55,7 +51,6 @@ scnorm <- function(x, mean = 0, sd = 1, which = c("mu", "sigma"), left = -Inf, r
   score
 }
 
-
 hcnorm <- function(x, mean = 0, sd = 1, which = c("mu", "sigma"), left = -Inf, right = Inf)
 {
   if(!is.character(which))
@@ -78,18 +73,20 @@ hcnorm <- function(x, mean = 0, sd = 1, which = c("mu", "sigma"), left = -Inf, r
           ifelse(  x <= left, - scorel * millsl - millsl^2,
             ifelse(x >= right,  scorer * millsr - millsr^2,
                                 -1 / sd2))
-
+    
     if(w == "sigma")
       hess[[w]] <-           
-        ifelse(x <= left,    ( 2 * dlm/sd2 - dlm^2/sd2*scorel)*millsl-millsl^2*dlm^2/sd2, 
-          ifelse(x >= right, (- 2 * drm/sd2 + drm^2/sd2*scorer)*millsr-millsr^2*drm^2/sd2,
-                             (sd2 - 3 * (x - mean)^2) / sd2^2)) 
+        ifelse(x <= left,    ( 2 * dlm/sd2 - dlm^2/sd2*scorel)*millsl-
+          millsl^2*dlm^2/sd2, 
+          ifelse(x >= right, (- 2 * drm/sd2 + drm^2/sd2*scorer)*millsr-
+            millsr^2*drm^2/sd2, (sd2 - 3 * (x - mean)^2) / sd2^2)) 
 
     if(w %in% c("mu.sigma", "sigma.mu"))
       hess[[w]] <- 
-        ifelse(x <= left,    (  1/sd - dlm/sd*scorel) * millsl - dlm/sd * millsl^2,
-          ifelse(x >= right, (- 1/sd + drm/sd*scorer) * millsr - drm/sd * millsr^2,
-                             -2 * dxm / sd^3))
+        ifelse(x <= left,    (  1/sd - dlm/sd*scorel) * millsl - 
+          dlm/sd * millsl^2,
+          ifelse(x >= right, (- 1/sd + drm/sd*scorer) * millsr - 
+            drm/sd * millsr^2, -2 * dxm / sd^3))
   }
 
   hess <- do.call("cbind", hess)
@@ -99,45 +96,3 @@ hcnorm <- function(x, mean = 0, sd = 1, which = c("mu", "sigma"), left = -Inf, r
   colnames(hess)[colnames(hess) == "dsigma"] <- "d2sigma"
   hess
 }
-
-
-## TODO: check if fcens correct!
-
-fcnorm <- function(x, mean = 0, sd = 1, which = c("mu", "sigma"), left = -Inf, right = Inf)
-{
-  if(!is.character(which))
-    which <- c("mu", "sigma", "mu.sigma", "sigma.mu")[as.integer(which)]
-  which <- tolower(which)
-  x <- data.frame(x = x, mean, sd)$x
-  fish <- list()
-  sd2 <- sd^2
-  dlm <- left - mean
-  drm <- right - mean
-  millsl <- dnorm(left, mean, sd)/pnorm(left, mean, sd)
-  millsr <- dnorm(right, mean, sd)/pnorm(right, mean, sd, lower.tail = FALSE)
-  for(w in which) {
-    if(w == "mu")
-      fish[[w]] <- 
-        ifelse(  x <= left,    dlm/sd2 * millsl + millsl^2,
-          ifelse(x >= right, - drm/sd2 * millsr + millsr^2,
-                               1 / sd2))
-    if(w == "sigma")
-      fish[[w]] <- 
-        ifelse(x <= left,    (  2 * dlm/sd2 - dlm^3/sd^3) * millsl - dlm^2/sd2 * millsl^2,
-          ifelse(x >= right, (- 2 * drm/sd2 + drm^3/sd^3) * millsr - drm^2/sd2 * millsr^2,
-                              2 / sd2))
-    if(w %in% c("mu.sigma", "sigma.mu"))
-      fish[[w]] <- 
-        ifelse(x <= left,    (  1/sd - dlm^2/sd^3) * millsl - dlm/sd * millsl^2,
-          ifelse(x >= right, (- 1/sd + drm^2/sd^3) * millsr - drm/sd * millsr^2,
-                             0))
-  }
-  fish <- do.call("cbind", fish)
-  colnames(fish) <- gsub("mu", "dmu", colnames(fish))
-  colnames(fish) <- gsub("sigma", "dsigma", colnames(fish))
-  colnames(fish)[colnames(fish) == "dmu"] <- "d2mu"
-  colnames(fish)[colnames(fish) == "dsigma"] <- "d2sigma"
-  fish
-}
-
-
