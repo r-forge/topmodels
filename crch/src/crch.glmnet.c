4df6e4bad0fceb658eec313f090babc722b59855
jakob <jakob@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-02-18 17:56:38 +0000

    added link function and moved derivation of lambdasequence to C
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/topmodels@860 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- crch/src/crch.glmnet.c
+++ crch/src/crch.glmnet.c
@@ -5,12 +5,9 @@
 
 
 
-void wobsmu(double *x, double *z, double *y, double *par, double *left, double *right, int p, int q, int n, double *wobs, double *hess) 
+void wobsmu(double *x, double *z, double *y, double *par, double *left, double *right, int p, int q, int n, double *wobs, double *hess, int linkfun) 
 {
   int i, j;
-
-
-
   double ddist, a, pdist, mills, mu, sigma;
 
   for(i = 0; i < n; i++) {
@@ -22,8 +19,13 @@ void wobsmu(double *x, double *z, double *y, double *par, double *left, double *
     for(j = 0; j < q; j++) {
       sigma = sigma + z[n*j+i]*par[p+j];
     }
-
-    sigma = exp(sigma);
+    if(linkfun == 1) {
+      sigma = exp(sigma);
+    } else if(linkfun == 2) {
+      sigma = sigma;
+    } else if(linkfun == 3) {
+      sigma = sqrt(sigma);
+    }
     if(y[i] <= left[i]) {
 
       ddist = dnorm((left[i] - mu) / sigma, 0.0, 1.0, 0) / sigma;
@@ -46,16 +48,14 @@ void wobsmu(double *x, double *z, double *y, double *par, double *left, double *
         hess[i] = 1 / pow(sigma, 2.0);
         wobs[i] = y[i];
       }
-    }
-
-    
+    }  
   }
 }
 
 
 
 
-void wobssigma(double *x, double *z, double *y, double *par, double *left, double *right, int p, int q, int n, double *wobs, double *hess) 
+void wobssigma(double *x, double *z, double *y, double *par, double *left, double *right, int p, int q, int n, double *wobs, double *hess, int linkfun) 
 {
   int i, j;
 
@@ -72,7 +72,13 @@ void wobssigma(double *x, double *z, double *y, double *par, double *left, doubl
     for(j = 0; j < q; j++) {
       sigma = sigma + z[n*j+i]*par[p+j];
     }
-    sigma = exp(sigma);
+    if(linkfun == 1) {
+      sigma = exp(sigma);
+    } else if(linkfun == 2) {
+      sigma = sigma;
+    } else if(linkfun == 3) {
+      sigma = sqrt(sigma);
+    }
     if(y[i] <= left[i]) {
       ddist = dnorm((left[i] - mu) / sigma, 0.0, 1.0, 0) / sigma;
       pdist = pnorm5((left[i] - mu) / sigma, 0.0, 1.0, 1, 0);
@@ -81,35 +87,43 @@ void wobssigma(double *x, double *z, double *y, double *par, double *left, doubl
       dcm = left[i] - mu;
       dcm2 = pow(dcm, 2.0);
       score = -1 * mills * (left[i] - mu);
-      hess[i] =  (-2 * dcm + dcm2*dcm/sd2)*mills + pow(mills, 2.0)*dcm2 - score*sigma;
-      wobs[i] = log(sigma) + 1/hess[i]*score;
+      hess[i] =  ((-2 * dcm + dcm2*dcm/sd2)*mills + pow(mills, 2.0)*dcm2)/sd2;
     } else {
       if(y[i] >= right[i]){
         ddist = dnorm((right[i] - mu) / sigma, 0.0, 1.0, 0) / sigma;
         pdist = pnorm5((right[i] - mu) / sigma, 0.0, 1.0, 0, 0);
         mills = ddist / pdist;
         sd2 = pow(sigma, 2.0);
-        dcm = left[i] - mu;
+        dcm = right[i] - mu;
         dcm2 = pow(dcm, 2.0);
-        score = mills * (right[i] - mu);
-        hess[i] = ( 2 * dcm - dcm2*dcm/sd2)*mills + pow(mills, 2.0)*dcm2- score*sigma;
-        wobs[i] = log(sigma) + 1/hess[i]*score;
+        score = mills * dcm/sigma;
+        hess[i] = (( 2 * dcm - dcm2*dcm/sd2)*mills + pow(mills, 2.0)*dcm2)/sd2;
       } else {
         sd2 = pow(sigma, 2.0);
         dcm2 = pow((y[i] - mu), 2.0);
-        score = (dcm2 - sd2) / pow(sigma, 2.0);
-        hess[i] = -(sd2 - 3 * dcm2)/sd2- score*sigma;
-        wobs[i] = log(sigma) + 1/hess[i]*score;
+        score = (dcm2 - sd2) / pow(sigma, 3.0);
+        hess[i] = -(sd2 - 3 * dcm2)/ pow(sd2, 2.0);
+        
       }
     }
-    
-    
+    if(linkfun == 1) {
+      score = score*sigma;
+      hess[i] = hess[i]*pow(sigma, 2.0)- score*sigma;
+      wobs[i] = log(sigma) + 1/hess[i]*score;
+    } else if(linkfun == 2) {
+      hess[i] = hess[i]- score;
+      wobs[i] = sigma + 1/hess[i]*score;
+    } else if(linkfun == 3) {
+      score = score/(2*sigma);
+      hess[i] = hess[i]/(4*pow(sigma, 2.0)) + score/(4*pow(sigma,2.0));
+      wobs[i] = pow(sigma, 2.0) + 1/hess[i]*score;
+    }
   }
 
 }
 
 
-double dcnorm2(double *x, double *z, double *y, double *par, double *left, double *right, int p, int q, int n)
+double dcnorm2(double *x, double *z, double *y, double *par, double *left, double *right, int p, int q, int n, int linkfun)
 {
   int i, j;
 
@@ -125,7 +139,13 @@ double dcnorm2(double *x, double *z, double *y, double *par, double *left, doubl
     for(j = 0; j < q; j++) {
       sigma = sigma + z[n*j+i]*par[p+j];
     }
-    sigma = exp(sigma);
+    if(linkfun == 1) {
+      sigma = exp(sigma);
+    } else if(linkfun == 2) {
+      sigma = sigma;
+    } else if(linkfun == 3) {
+      sigma = sqrt(sigma);
+    }
     if(y[i] <= left[i]) {
       dens = pnorm5((left[i] - mu) / sigma, 0.0, 1.0, 1, 1);
     } else {
@@ -142,46 +162,93 @@ double dcnorm2(double *x, double *z, double *y, double *par, double *left, doubl
 }
 
 
-SEXP crchglmnet(SEXP x, SEXP z, SEXP y, SEXP left, SEXP right, SEXP lambdaseq, SEXP maxit, SEXP reltol)
+SEXP crchglmnet(SEXP x, SEXP z, SEXP y, SEXP left, SEXP right, SEXP lambdasequ, SEXP nlambdau, SEXP lambdaminratio, SEXP maxit, SEXP reltol, SEXP linkfun)
 {
   int n = INTEGER(GET_DIM(x))[0];
   int p = INTEGER(GET_DIM(x))[1];
   int q = INTEGER(GET_DIM(z))[1];
-  int nlambda = length(lambdaseq);
+  int nlambda = length(lambdasequ);
 
-  SEXP coefpath = PROTECT(allocMatrix(REALSXP, nlambda, p+q));
-  SEXP llpath = PROTECT(allocVector(REALSXP, nlambda));
-  SEXP rval = PROTECT(allocVector(VECSXP, 2));
-  SET_VECTOR_ELT(rval, 0, coefpath);
-  SET_VECTOR_ELT(rval, 1, llpath);
-
-  double *coefpathptr = REAL(coefpath);
-  double *llpathptr = REAL(llpath);
+  
 
   double *yptr = REAL(y);
   double *xptr = REAL(x);
   double *zptr = REAL(z);
   double *leftptr = REAL(left);
   double *rightptr = REAL(right);
-  double *lambdaseqptr = REAL(lambdaseq);
+  double *lambdasequptr = REAL(lambdasequ);
+  int *nlambdauptr = INTEGER(nlambdau);
   int *maxitptr = INTEGER(maxit);
   double *reltolptr = REAL(reltol);
+  int *linkfunptr = INTEGER(linkfun);
+  double *lambdaminratioptr = REAL(lambdaminratio);
+
+  SEXP coefpath = PROTECT(allocMatrix(REALSXP, *nlambdauptr, p+q));
+  SEXP llpath = PROTECT(allocVector(REALSXP, *nlambdauptr));
+  SEXP lambdaseq = PROTECT(allocVector(REALSXP, *nlambdauptr));
+  SEXP rval = PROTECT(allocVector(VECSXP, 3));
+  SET_VECTOR_ELT(rval, 0, coefpath);
+  SET_VECTOR_ELT(rval, 1, llpath);
+  SET_VECTOR_ELT(rval, 2, lambdaseq);
+
+  double *coefpathptr = REAL(coefpath);
+  double *llpathptr = REAL(llpath);
+  double *lambdaseqptr = REAL(lambdaseq);
 
   int i, lambdaind, it1, it2, colind, rowind, colind2;
   int activex[p], activez[q];
-  double a, xbeta, coefold, zgamma, dev, devold, ll, llold, lambda, coef[p+q];
-  double wxsum[p], wzsum[q], resid[n], w[n], wobs[n];
+  double a, xbeta, coefold, zgamma, dev, devold, ll, llold, lambda, lambdamax, xysum;
+  double wxsum[p], wzsum[q], resid[n], w[n], wobs[n], coef[p+q];
+
 
 
   for(i = 0; i < p+q; i++) {
     coef[i] = 0;
   }
+  if(*linkfunptr == 2 || *linkfunptr ==3) {
+    coef[p] = 1;
+  }
+
+  if(nlambda == 1 & lambdasequptr[0] < 0) {
+    nlambda = *nlambdauptr;
+    wobsmu(xptr, zptr, yptr, coef, leftptr, rightptr, p, q, n, wobs, w, *linkfunptr);
+    for(colind = 0; colind < p; colind++) {
+      xysum = 0;
+      wxsum[colind] = 0;
+      for(rowind = 0; rowind < n; rowind++) {
+        xysum = xysum + w[rowind]*xptr[rowind+colind*n]*wobs[rowind];
+        wxsum[colind] = wxsum[colind] + w[rowind]*pow(xptr[rowind+colind*n], 2);
+      }
+      lambdamax = fmax(fabs(xysum/wxsum[colind]), lambdamax);
+    }
+    wobssigma(xptr, zptr, yptr, coef, leftptr, rightptr, p, q, n, wobs, w, *linkfunptr);
+    for(colind = 0; colind < q; colind++) {
+      xysum = 0;
+      wzsum[colind] = 0;
+      for(rowind = 0; rowind < n; rowind++) {
+        xysum = xysum + w[rowind]*zptr[rowind+colind*n]*wobs[rowind];
+        wzsum[colind] = wzsum[colind] + w[rowind]*pow(zptr[rowind+colind*n], 2);
+      }
+      lambdamax = fmax(fabs(xysum/wzsum[colind]), lambdamax);
+    }
+    for(i = 0; i<nlambda; i++) {
+      lambdaseqptr[i] = exp(-log(*lambdaminratioptr)/(nlambda-1)*(nlambda-1-i) + log(*lambdaminratioptr*lambdamax));
+
+    }
+
+  } else {
+    for(i = 0; i<nlambda; i++) {
+      lambdaseqptr[i] = lambdasequptr[i];
+    }
+  }
+  
+
 
   for(lambdaind = 0; lambdaind < nlambda; lambdaind++) {
     lambda = lambdaseqptr[lambdaind];
     for(it1 = 0; it1 < *maxitptr; it1++) {
       /* location */
-      wobsmu(xptr, zptr, yptr, coef, leftptr, rightptr, p, q, n, wobs, w);
+      wobsmu(xptr, zptr, yptr, coef, leftptr, rightptr, p, q, n, wobs, w, *linkfunptr);
       for(i = 0; i < p; i++) {
         activex[i] = 1;
       }
@@ -240,7 +307,7 @@ SEXP crchglmnet(SEXP x, SEXP z, SEXP y, SEXP left, SEXP right, SEXP lambdaseq, S
       }
  
       /* log-lik */
-      ll = dcnorm2(xptr, zptr, yptr, coef, leftptr, rightptr, p, q, n);
+      ll = dcnorm2(xptr, zptr, yptr, coef, leftptr, rightptr, p, q, n, *linkfunptr);
       if(fabs((ll-llold)/llold) < *reltolptr) {
         break;
       }
@@ -250,7 +317,7 @@ SEXP crchglmnet(SEXP x, SEXP z, SEXP y, SEXP left, SEXP right, SEXP lambdaseq, S
     /* scale */
     for(it1 = 0; it1 < *maxitptr; it1++) {
 
-      wobssigma(xptr, zptr, yptr, coef, leftptr, rightptr, p, q, n, wobs, w);
+      wobssigma(xptr, zptr, yptr, coef, leftptr, rightptr, p, q, n, wobs, w, *linkfunptr);
       activez[0] = 2;   /* exclude intercept from regularization TODO: what to do if no intercept */
       for(i = 1; i < q; i++) {
         activez[i] = 1;
@@ -312,7 +379,7 @@ SEXP crchglmnet(SEXP x, SEXP z, SEXP y, SEXP left, SEXP right, SEXP lambdaseq, S
         devold = dev;
       }
 
-      ll = dcnorm2(xptr, zptr, yptr, coef, leftptr, rightptr, p, q, n);
+      ll = dcnorm2(xptr, zptr, yptr, coef, leftptr, rightptr, p, q, n, *linkfunptr);
       if(fabs((ll-llold)/llold) < *reltolptr) {
         break;
       }
@@ -321,7 +388,7 @@ SEXP crchglmnet(SEXP x, SEXP z, SEXP y, SEXP left, SEXP right, SEXP lambdaseq, S
     llpathptr[lambdaind] = ll;
   }
 
- UNPROTECT(3);
+ UNPROTECT(4);
  return rval;
 }
 
