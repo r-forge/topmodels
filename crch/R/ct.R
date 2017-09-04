b56f67d449a543b0da250289e9a675ac76904917
jakob <jakob@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-09-04 09:22:59 +0000

    changed argument names mean and sd to location and scale
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/topmodels@1059 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- crch/R/ct.R
+++ crch/R/ct.R
@@ -1,8 +1,8 @@
 ## density
-dct <- function(x, mean = 0, sd = 1, df, left = -Inf, right = Inf, log = FALSE) {
-  input <- data.frame(x = as.numeric(x), mean = as.numeric(mean), sd = as.numeric(sd), 
+dct <- function(x, location = 0, scale = 1, df, left = -Inf, right = Inf, log = FALSE) {
+  input <- data.frame(x = as.numeric(x), location = as.numeric(location), scale = as.numeric(scale), 
     left = as.numeric(left), right = as.numeric(right))
-  rval <- with(input, .Call("dct", x, mean, sd, df, left, right, log))
+  rval <- with(input, .Call("dct", x, location, scale, df, left, right, log))
   if(is.matrix(x)) {
     rval <- matrix(rval, ncol = ncol(x), nrow = nrow(x))
     colnames(rval) <- colnames(x)
@@ -12,11 +12,11 @@ dct <- function(x, mean = 0, sd = 1, df, left = -Inf, right = Inf, log = FALSE) 
 }
 
 ## distribution function
-pct <- function(q, mean = 0, sd = 1, df, left = -Inf, right = Inf, 
+pct <- function(q, location = 0, scale = 1, df, left = -Inf, right = Inf, 
   lower.tail = TRUE, log.p = FALSE) {
-  input <- data.frame(q = as.numeric(q), mean = as.numeric(mean), sd = as.numeric(sd), 
+  input <- data.frame(q = as.numeric(q), location = as.numeric(location), scale = as.numeric(scale), 
     left = as.numeric(left), right = as.numeric(right))
-  rval <- with(input, .Call("pct", q, mean, sd, df, left, right, lower.tail, log.p))
+  rval <- with(input, .Call("pct", q, location, scale, df, left, right, lower.tail, log.p))
   if(is.matrix(q)) {
     rval <- matrix(rval, ncol = ncol(q), nrow = nrow(q))
     colnames(rval) <- colnames(q)
@@ -26,15 +26,15 @@ pct <- function(q, mean = 0, sd = 1, df, left = -Inf, right = Inf,
 }
 
 ## random numbers
-rct <- function(n, mean = 0, sd = 1, df, left = -Inf, right = Inf) {
-  rval <- rt(n, df = df) * sd + mean
+rct <- function(n, location = 0, scale = 1, df, left = -Inf, right = Inf) {
+  rval <- rt(n, df = df) * scale + location
   pmax(pmin(rval, right), left)
 }
 
 ## quantiles
-qct <- function(p, mean = 0, sd = 1, df, left = -Inf, right = Inf,
+qct <- function(p, location = 0, scale = 1, df, left = -Inf, right = Inf,
   lower.tail = TRUE, log.p = FALSE) {
-  rval <- qt(p, df = df, lower.tail = lower.tail, log.p = log.p) * sd + mean
+  rval <- qt(p, df = df, lower.tail = lower.tail, log.p = log.p) * scale + location
   rval <- pmax(pmin(rval, right), left)
   if(is.matrix(p)) {
     rval <- matrix(rval, ncol = ncol(p), nrow = nrow(p))
@@ -46,9 +46,9 @@ qct <- function(p, mean = 0, sd = 1, df, left = -Inf, right = Inf,
 
 
 ## scores
-sct <- function(x, mean = 0, sd = 1, df, left = -Inf, right = Inf, 
+sct <- function(x, location = 0, scale = 1, df, left = -Inf, right = Inf, 
   which = c("mu", "sigma")) {
-  input <- data.frame(x = as.numeric(x), mean = as.numeric(mean), sd = as.numeric(sd), 
+  input <- data.frame(x = as.numeric(x), location = as.numeric(location), scale = as.numeric(scale), 
     df = as.numeric(df), left = as.numeric(left), right = as.numeric(right))
   if(!is.character(which))
     which <- c("mu", "sigma")[as.integer(which)]
@@ -57,9 +57,9 @@ sct <- function(x, mean = 0, sd = 1, df, left = -Inf, right = Inf,
   
   for(w in which) {
     if(w == "mu")
-      score2 <- with(input, .Call("sct_mu", x, mean, sd, df, left, right))
+      score2 <- with(input, .Call("sct_mu", x, location, scale, df, left, right))
     if(w == "sigma")
-      score2 <- with(input, .Call("sct_sigma", x, mean, sd, df, left, right))
+      score2 <- with(input, .Call("sct_sigma", x, location, scale, df, left, right))
     score <- cbind(score, score2)
   }
   if(is.null(dim(score)))
@@ -70,9 +70,9 @@ sct <- function(x, mean = 0, sd = 1, df, left = -Inf, right = Inf,
 
 
 ## Hessian
-hct <- function(x, mean = 0, sd = 1, df, left = -Inf, right = Inf, 
+hct <- function(x, location = 0, scale = 1, df, left = -Inf, right = Inf, 
   which = c("mu", "sigma")) {
-  input <- data.frame(x = as.numeric(x), mean = as.numeric(mean), sd = as.numeric(sd), 
+  input <- data.frame(x = as.numeric(x), location = as.numeric(location), scale = as.numeric(scale), 
     df = as.numeric(df), left = as.numeric(left), right = as.numeric(right))
   if(!is.character(which))
     which <- c("mu", "sigma", "mu.sigma", "sigma.mu")[as.integer(which)]
@@ -80,11 +80,11 @@ hct <- function(x, mean = 0, sd = 1, df, left = -Inf, right = Inf,
   hess <- list()
   for(w in which) {       
     if(w == "mu")         
-      hess[[w]] <- with(input, .Call("hct_mu", x, mean, sd, df, left, right))  
+      hess[[w]] <- with(input, .Call("hct_mu", x, location, scale, df, left, right))  
     if(w == "sigma")
-      hess[[w]] <- with(input, .Call("hct_sigma", x, mean, sd, df, left, right))  
+      hess[[w]] <- with(input, .Call("hct_sigma", x, location, scale, df, left, right))  
     if(w %in% c("mu.sigma", "sigma.mu"))
-      hess[[w]] <- with(input, .Call("hct_musigma", x, mean, sd, df, left, right))  
+      hess[[w]] <- with(input, .Call("hct_musigma", x, location, scale, df, left, right))  
   }
 
   hess <- do.call("cbind", hess)
@@ -97,11 +97,11 @@ hct <- function(x, mean = 0, sd = 1, df, left = -Inf, right = Inf,
 
 
 ## Expectation
-ect <- function(mean = 0, sd = 1, df, left = -Inf, right = Inf) {
-  rmm <- (right-mean)/sd
-  lmm <- (left-mean)/sd
+ect <- function(location = 0, scale = 1, df, left = -Inf, right = Inf) {
+  rmm <- (right-location)/scale
+  lmm <- (left-location)/scale
   pncens <- pt(rmm, df = df)-pt(lmm, df = df)
-  pncens*ett(mean = mean, sd = sd, df = df, left = left, right = right) + 
+  pncens*ett(location = location, scale = scale, df = df, left = left, right = right) + 
     pt(lmm, df = df)*left^(is.finite(left)) + 
     pt(rmm, df = df, lower.tail = FALSE)*right^(is.finite(right))
 }
