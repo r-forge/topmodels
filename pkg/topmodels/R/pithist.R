192a06463eaeda976848d47005a6f59c5d31d9bc
mlang <mlang@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-02-25 16:44:45 +0000

    Improve solely (!) coding style of `pithist()`
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/topmodels@1177 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/topmodels/R/pithist.R
+++ pkg/topmodels/R/pithist.R
@@ -26,22 +26,22 @@ pithist <- function(object, ...) {
 }
 
 
-pithist.default <- function(object, 
-                            newdata = NULL, 
-                            plot = TRUE, 
+pithist.default <- function(object,
+                            newdata = NULL,
+                            plot = TRUE,
                             style = c("histogram", "lines"),
-                            type = c("random", "proportional"), 
-                            nsim = 1L, 
-                            freq = FALSE, 
-                            breaks = NULL, 
-                            xlim = c(0, 1), 
-                            ylim = NULL, 
-                            main = NULL, 
-                            xlab = "PIT", 
-                            ylab = if(freq) "Frequency" else "Density",
-                            confint = TRUE, 
-                            confint_level = 0.95, 
-                            confint_type = c("exact", "approximation"), 
+                            type = c("random", "proportional"),
+                            nsim = 1L,
+                            freq = FALSE,
+                            breaks = NULL,
+                            xlim = c(0, 1),
+                            ylim = NULL,
+                            main = NULL,
+                            xlab = "PIT",
+                            ylab = if (freq) "Frequency" else "Density",
+                            confint = TRUE,
+                            confint_level = 0.95,
+                            confint_type = c("exact", "approximation"),
                             ...) {
 
   ## match arguments
@@ -50,56 +50,61 @@ pithist.default <- function(object,
 
   ## either compute proportion exactly (to do...) or approximate by simulation
   type <- match.arg(type, c("random", "proportional"))
-  if(type == "proportional") {
+  if (type == "proportional") {
     stop("not yet implemented")
   } else {
-    # TODO: (ML) What is the default fun for? 
+    # TODO: (ML) What is the default fun for?
     p <- qresiduals.default(object, newdata = newdata, trafo = NULL, type = "random", nsim = nsim)
   }
 
   ## breaks
-  if(is.null(breaks)) breaks <- c(4, 10, 20, 25)[cut(NROW(p), c(0, 50, 5000, 1000000, Inf))]
-  if(length(breaks) == 1L) breaks <- seq(xlim[1L], xlim[2L], length.out = breaks + 1L)
+  if (is.null(breaks)) breaks <- c(4, 10, 20, 25)[cut(NROW(p), c(0, 50, 5000, 1000000, Inf))]
+  if (length(breaks) == 1L) breaks <- seq(xlim[1L], xlim[2L], length.out = breaks + 1L)
 
   ## ci interval
   if (confint_type == "exact") {
     ci <- get_confint(NROW(p), length(breaks) - 1, confint_level, freq)
   } else {
     ci <- get_confint_agresti(
-      NROW(p) / (length(breaks) - 1), 
-      NROW(p), 
-      confint_level, 
-      length(breaks) - 1, 
-      freq)
+      NROW(p) / (length(breaks) - 1),
+      NROW(p),
+      confint_level,
+      length(breaks) - 1,
+      freq
+    )
   }
 
   ## perfect prediction
   pp <- ifelse(freq, NROW(p) / (length(breaks) - 1), 1)
 
   ## labels
-  if(is.null(main)) main <- deparse(substitute(object))
+  if (is.null(main)) main <- deparse(substitute(object))
 
   ## collect everything as data.frame
-  ## TODO: (ML) Should it really be prepared as a `data.frame` and 
+  ## TODO: (ML) Should it really be prepared as a `data.frame` and
   ## return name of hist() should be renamed
   ## TODO: (ML) Maybe get rid of `hist()`
   tmp_hist <- hist(p, breaks = breaks, plot = FALSE)
   if (freq) {
-    rval <- data.frame(x = tmp_hist$mids, 
-                       xleft = tmp_hist$breaks[-length(tmp_hist$breaks)], 
-                       xright = tmp_hist$breaks[-1L], 
-                       counts = tmp_hist$counts, 
-                       ci_lower = ci[1], 
-                       ci_upper = ci[2], 
-                       pp = pp) 
+    rval <- data.frame(
+      x = tmp_hist$mids,
+      xleft = tmp_hist$breaks[-length(tmp_hist$breaks)],
+      xright = tmp_hist$breaks[-1L],
+      counts = tmp_hist$counts,
+      ci_lower = ci[1],
+      ci_upper = ci[2],
+      pp = pp
+    )
   } else {
-    rval <- data.frame(x = tmp_hist$mids, 
-                       xleft = tmp_hist$breaks[-length(tmp_hist$breaks)], 
-                       xright = tmp_hist$breaks[-1L], 
-                       density = tmp_hist$density, 
-                       ci_lower = ci[1], 
-                       ci_upper = ci[2], 
-                       pp = pp) 
+    rval <- data.frame(
+      x = tmp_hist$mids,
+      xleft = tmp_hist$breaks[-length(tmp_hist$breaks)],
+      xright = tmp_hist$breaks[-1L],
+      density = tmp_hist$density,
+      ci_lower = ci[1],
+      ci_upper = ci[2],
+      pp = pp
+    )
   }
   attr(rval, "xlab") <- xlab
   attr(rval, "ylab") <- ylab
@@ -108,8 +113,10 @@ pithist.default <- function(object,
 
   ## also plot by default
   if (plot) {
-    plot(rval, style = style, xlim = xlim, ylim = ylim, xlab = xlab, ylab = ylab, 
-      confint = confint, ...)
+    plot(rval,
+      style = style, xlim = xlim, ylim = ylim, xlab = xlab, ylab = ylab,
+      confint = confint, ...
+    )
   }
 
   ## return invisibly
@@ -123,25 +130,25 @@ c.pithist <- rbind.pithist <- function(...) {
   rval <- list(...)
 
   ## group sizes
-  for(i in seq_along(rval)) {
-    if(is.null(rval[[i]]$group)) rval[[i]]$group <- 1L
-  } 
+  for (i in seq_along(rval)) {
+    if (is.null(rval[[i]]$group)) rval[[i]]$group <- 1L
+  }
   n <- lapply(rval, function(r) table(r$group))
 
   ## labels
   xlab <- unlist(lapply(rval, function(r) attr(r, "xlab")))
   ylab <- unlist(lapply(rval, function(r) attr(r, "ylab")))
   nam <- names(rval)
-  main <- if(is.null(nam)) {
+  main <- if (is.null(nam)) {
     as.vector(sapply(rval, function(r) attr(r, "main")))
   } else {
     make.unique(rep.int(nam, sapply(n, length)))
-  } 
+  }
   n <- unlist(n)
 
   ## combine and return
   rval <- do.call("rbind.data.frame", rval)
-  rval$group <- if(length(n) < 2L) NULL else rep.int(seq_along(n), n)
+  rval$group <- if (length(n) < 2L) NULL else rep.int(seq_along(n), n)
   attr(rval, "xlab") <- xlab
   attr(rval, "ylab") <- ylab
   attr(rval, "main") <- main
@@ -151,41 +158,39 @@ c.pithist <- rbind.pithist <- function(...) {
 
 
 plot.pithist <- function(x,
-                         style = c("histogram", "lines"), 
-                         confint = TRUE,  
-                         xlim = NULL, 
-                         ylim = NULL, 
-                         xlab = NULL, 
-                         ylab = NULL, 
+                         style = c("histogram", "lines"),
+                         confint = TRUE,
+                         xlim = NULL,
+                         ylim = NULL,
+                         xlab = NULL,
+                         ylab = NULL,
                          main = NULL,
-                         border = "black", 
-                         fill = adjustcolor(1, alpha.f = 0.2), 
+                         border = "black",
+                         fill = adjustcolor(1, alpha.f = 0.2),
                          col = c(1, 1),
-                         lwd = 2, 
-                         lty = c(1, 2), 
-                         axes = TRUE, 
+                         lwd = 2,
+                         lty = c(1, 2),
+                         axes = TRUE,
                          ...) {
-
   style <- match.arg(style, c("histogram", "lines"))
 
   ## handling of groups
-  if(is.null(x$group)) x$group <- 1L
-  n <- max(x$group) 
-  
+  if (is.null(x$group)) x$group <- 1L
+  n <- max(x$group)
+
   ## annotation
-  if(is.null(xlab)) xlab <- TRUE
-  if(is.null(ylab)) ylab <- TRUE
-  if(is.null(main)) main <- TRUE 
+  if (is.null(xlab)) xlab <- TRUE
+  if (is.null(ylab)) ylab <- TRUE
+  if (is.null(main)) main <- TRUE
   xlab <- rep(xlab, length.out = n)
   ylab <- rep(ylab, length.out = n)
   main <- rep(main, length.out = n)
-  if(is.logical(xlab)) xlab <- ifelse(xlab, attr(x, "xlab"), "")
-  if(is.logical(ylab)) ylab <- ifelse(ylab, attr(x, "ylab"), "")
-  if(is.logical(main)) main <- ifelse(main, attr(x, "main"), "")
+  if (is.logical(xlab)) xlab <- ifelse(xlab, attr(x, "xlab"), "")
+  if (is.logical(ylab)) ylab <- ifelse(ylab, attr(x, "ylab"), "")
+  if (is.logical(main)) main <- ifelse(main, attr(x, "main"), "")
 
   ## plotting function
   pithist1 <- function(d, ...) {
-
     if (length(col) < 2) col <- rep(col[1], 2)
     if (length(lty) < 2) lty <- rep(lty[1], 2)
 
@@ -196,23 +201,25 @@ plot.pithist <- function(x,
     j <- unique(d$group)
     ci_lower <- ifelse(confint, unique(d$ci_lower), NULL)
     ci_upper <- ifelse(confint, unique(d$ci_upper), NULL)
-    pp <-  unique(d$pp)
+    pp <- unique(d$pp)
 
     ## defaults
-    if(is.null(xlim)) xlim <- range(c(xleft, xright))
-    if(is.null(ylim)) ylim <- range(c(0, y, ci_lower, ci_upper))
+    if (is.null(xlim)) xlim <- range(c(xleft, xright))
+    if (is.null(ylim)) ylim <- range(c(0, y, ci_lower, ci_upper))
 
-    ## draw pithist 
-    plot(0, 0, type = "n", xlim = xlim, ylim = ylim,
-      xlab = xlab[j], ylab = ylab[j], main = main[j], axes = FALSE, ...)
-    if(axes) {
+    ## draw pithist
+    plot(0, 0,
+      type = "n", xlim = xlim, ylim = ylim,
+      xlab = xlab[j], ylab = ylab[j], main = main[j], axes = FALSE, ...
+    )
+    if (axes) {
       axis(1)
       axis(2)
     }
     rect(xleft, 0, xright, y, border = border, col = fill)
     abline(h = pp, col = col[1], lty = lty[1], lwd = lwd)
-    if(confint) abline(h = ci_lower, col = col[2], lty = lty[2], lwd = lwd)
-    if(confint) abline(h = ci_upper, col = col[2], lty = lty[2], lwd = lwd)
+    if (confint) abline(h = ci_lower, col = col[2], lty = lty[2], lwd = lwd)
+    if (confint) abline(h = ci_upper, col = col[2], lty = lty[2], lwd = lwd)
   }
 
   ## plotting function
@@ -224,30 +231,35 @@ plot.pithist <- function(x,
     j <- unique(d$group)
     ci_lower <- if (confint) unique(d$ci_lower) else NULL
     ci_upper <- if (confint) unique(d$ci_upper) else NULL
-    pp <-  unique(d$pp)
+    pp <- unique(d$pp)
 
     ## defaults
-    if(is.null(xlim)) xlim <- range(x)
-    if(is.null(ylim)) ylim <- range(c(0, y, ci_lower, ci_upper))
-    ## draw pithist 
-    plot(0, 0, type = "n", xlim = xlim, ylim = ylim,
-      xlab = xlab[j], ylab = ylab[j], main = main[j], axes = FALSE, ...)
-    if(axes) {
+    if (is.null(xlim)) xlim <- range(x)
+    if (is.null(ylim)) ylim <- range(c(0, y, ci_lower, ci_upper))
+    ## draw pithist
+    plot(0, 0,
+      type = "n", xlim = xlim, ylim = ylim,
+      xlab = xlab[j], ylab = ylab[j], main = main[j], axes = FALSE, ...
+    )
+    if (axes) {
       axis(1)
       axis(2)
     }
 
-    if(confint) polygon(c(0, 1, 1, 0), c(ci_lower, ci_lower, ci_upper, ci_upper),
-      col = fill, border = NA)
-    #abline(h = pp, col = col, lty = lty[1], lwd = lwd)
+    if (confint) {
+      polygon(c(0, 1, 1, 0), c(ci_lower, ci_lower, ci_upper, ci_upper),
+        col = fill, border = NA
+      )
+    }
+    # abline(h = pp, col = col, lty = lty[1], lwd = lwd)
     lines(y ~ x, type = "s", lwd = lwd, lty = lty[1], col = col[1])
   }
 
   ## draw plots
-  if(n > 1L) par(mfrow = n2mfrow(n))
+  if (n > 1L) par(mfrow = n2mfrow(n))
   for (i in 1L:n) {
     if (style == "histogram") {
-      pithist1(x[x$group == i, ], ...) 
+      pithist1(x[x$group == i, ], ...)
     } else {
       pithist2(x[x$group == i, ], ...)
     }
@@ -255,24 +267,24 @@ plot.pithist <- function(x,
 }
 
 
-lines.pithist <- function(x, 
-                          confint = FALSE,  
-                          fill = adjustcolor(1, alpha.f = 0.2), 
+lines.pithist <- function(x,
+                          confint = FALSE,
+                          fill = adjustcolor(1, alpha.f = 0.2),
                           col = 1,
-                          lwd = 2, 
-                          lty = 1,  
+                          lwd = 2,
+                          lty = 1,
                           ...) {
 
   ## handling of groups
-  if(is.null(x$group)) x$group <- 1L
-  n <- max(x$group) 
+  if (is.null(x$group)) x$group <- 1L
+  n <- max(x$group)
 
   ## Get right length of `col` and `lty`
   if (length(col) < n) col <- rep(col[1], n)
   if (length(lty) < n) lty <- rep(lty[1], n)
   if (length(lwd) < n) lwd <- rep(lwd[1], n)
   if (length(fill) < n) fill <- rep(fill[1], n)
-  
+
   ## plotting function
   pithist2 <- function(d, col, lty, lwd, fill, ...) {
     ## rect elements
@@ -284,9 +296,12 @@ lines.pithist <- function(x,
     ci_lower <- if (confint) unique(d$ci_lower) else NULL
     ci_upper <- if (confint) unique(d$ci_upper) else NULL
 
-    ## draw pithist 
-    if(confint) polygon(c(0, 1, 1, 0), c(ci_lower, ci_lower, ci_upper, ci_upper),
-      col = fill, border = NA)
+    ## draw pithist
+    if (confint) {
+      polygon(c(0, 1, 1, 0), c(ci_lower, ci_lower, ci_upper, ci_upper),
+        col = fill, border = NA
+      )
+    }
     lines(y ~ x, type = "s", col = col, lty = lty, lwd = lwd, ...)
   }
 
@@ -299,25 +314,27 @@ lines.pithist <- function(x,
 
 ## ggplot2 interface
 autoplot.pithist <- function(object,
-                             colour = c("black", "#B61A51"), 
-                             fill = "darkgray", 
-                             size = 1.2, 
+                             colour = c("black", "#B61A51"),
+                             fill = "darkgray",
+                             size = 1.2,
                              ...) {
 
   ## determine grouping
   class(object) <- "data.frame"
-  if(is.null(object$group)) object$group <- 1L
+  if (is.null(object$group)) object$group <- 1L
   n <- max(object$group)
-  object$group <- factor(object$group, levels = 1L:n, 
-    labels = make.names(attr(object, "main"), unique = TRUE))
-  
+  object$group <- factor(object$group,
+    levels = 1L:n,
+    labels = make.names(attr(object, "main"), unique = TRUE)
+  )
+
   ## rectangles and fitted lines
-  rval <- ggplot2::ggplot(object, ggplot2::aes_string(xmin = "xleft", xmax = "xright", ymin = 0, ymax = "y")) + 
-    ggplot2::geom_rect(colour = colour[1L], fill = fill) + 
+  rval <- ggplot2::ggplot(object, ggplot2::aes_string(xmin = "xleft", xmax = "xright", ymin = 0, ymax = "y")) +
+    ggplot2::geom_rect(colour = colour[1L], fill = fill) +
     ggplot2::geom_hline(yintercept = 1, colour = colour[2L], size = size)
-  
+
   ## grouping (if any)
-  if(n > 1L) rval <- rval + ggplot2::facet_grid(group ~ .)
+  if (n > 1L) rval <- rval + ggplot2::facet_grid(group ~ .)
 
   ## annotation
   rval <- rval + ggplot2::xlab(paste(unique(attr(object, "xlab")), collapse = "/")) +
@@ -333,7 +350,7 @@ get_confint <- function(n, bins, level, freq) {
   a <- (1 - level) / 2
   a <- c(a, 1 - a)
   rval <- qbinom(a, size = n, prob = 1 / bins)
-  if(!freq) rval <- rval / (n / bins) 
+  if (!freq) rval <- rval / (n / bins)
   rval
 }
 
@@ -342,26 +359,27 @@ get_confint <- function(n, bins, level, freq) {
 ## doi=10.1080/00031305.1998.10480550
 get_confint_agresti <- function(x, n, level, bins, freq) {
   rval <- add4ci(x, n, level)$conf.int * n
-  if(!freq) rval <- rval / (n / bins) 
+  if (!freq) rval <- rval / (n / bins)
   rval
 }
 
 
 ## copy of `add4ci` package from package `PropCIs` by Ralph Scherer (licensed under GPL-2/GPL-3)
-add4ci <- function (x, n, conf.level) {
-  ptilde = (x + 2)/(n + 4)
-  z = abs(qnorm((1 - conf.level)/2))
-  stderr = sqrt(ptilde * (1 - ptilde)/(n + 4))
-  ul = ptilde + z * stderr
-  ll = ptilde - z * stderr
-  if (ll < 0) 
-      ll = 0
-  if (ul > 1) 
-      ul = 1
+add4ci <- function(x, n, conf.level) {
+  ptilde <- (x + 2) / (n + 4)
+  z <- abs(qnorm((1 - conf.level) / 2))
+  stderr <- sqrt(ptilde * (1 - ptilde) / (n + 4))
+  ul <- ptilde + z * stderr
+  ll <- ptilde - z * stderr
+  if (ll < 0) {
+    ll <- 0
+  }
+  if (ul > 1) {
+    ul <- 1
+  }
   cint <- c(ll, ul)
   attr(cint, "conf.level") <- conf.level
   rval <- list(conf.int = cint, estimate = ptilde)
   class(rval) <- "htest"
   return(rval)
 }
-
