50ed2ce58545ed931474cd1f8182237f69af6ec8
mlang (Moritz Lang) <moritz.n.lang@gmail.com>
2021-03-25 21:16:48 +0000

    Remove censoring/truncation attributes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/topmodels@1213 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/topmodels/R/procast.R
+++ pkg/topmodels/R/procast.R
@@ -232,7 +232,7 @@ procast.crch <- function(object,
     }
   }
 
-  attr(rval, "cens") <- list(left = object$cens$left, right = object$cens$right)
+  # attr(rval, "cens") <- list(left = object$cens$left, right = object$cens$right)
 
   return(rval)
 }
