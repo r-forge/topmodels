380899a9684f62f6ed5e2a7f226ca4b0702f9077
mlang <mlang@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-04-15 17:45:31 +0000

    Improve reliagram
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/topmodels@1223 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/topmodels/R/reliagram.R
+++ pkg/topmodels/R/reliagram.R
@@ -42,15 +42,15 @@ reliagram.default <- function(object,
   if(length(prob) == 1) prob <- c((0.5 - prob/2), (0.5 + prob/2))
   if(max(prob) >= 1 | min(prob) <= 0) stop("confidence interval probabilities outside [0,1]")
 
-  ### data + thresholds
-  #y <- if(is.null(y)) newresponse(object, newdata = newdata) else as.numeric(y)
-  #thresholds <- if(is.null(thresholds)) median(y, na.rm = TRUE) else as.numeric(thresholds)
+  ## data + thresholds
+  y <- if(is.null(y)) newresponse(object, newdata = newdata) else as.numeric(y)
+  thresholds <- if(is.null(thresholds)) median(y, na.rm = TRUE) else as.numeric(thresholds)
 
-  ### predicted probabilities
-  #pred <- procast(object, newdata = newdata, type = "probability", at = matrix(thresholds, nrow = 1L),
-  #  drop = TRUE)
+  ## predicted probabilities
+  pred <- procast(object, newdata = newdata, type = "probability", at = matrix(thresholds, nrow = 1L),
+    drop = TRUE)
 
-  ## compute quantities to be plotted
+  ## TODO: (Z) compute quantities to be plotted
   #rval <- data.frame("<cut prob>", "<aggregate y by prob and thresh>")
 
   ## get/prepare observations
@@ -60,36 +60,29 @@ reliagram.default <- function(object,
   ## define convenience variables
   N <- NROW(obs)
 
-  ## use function brier from verification package to compute 
-  ## observed relative frequencies (obar.i) and bin-centers (y.i)
-  temp   <- brier(obs = obs, pred = pred, thresholds = breaks, bins = bins)
+  ## compute observed relative frequencies (obar.i) and bin-centers (y.i)
+  temp <- brier(obs = obs, pred = pred, thresholds = breaks, bins = bins)
 
-  minimumIndex <- which(temp$prob.y * N >= minimum)  # FIXME: (ML) What is that for?
-
-  ## mean values of pred in the intervals
-  # y.i <- temp$y.i  # FIXME: (ML) Why not used instead of for loop?
-  y.i <- NULL
-  for (i in 1:(length(breaks) - 1)) {
-    y.i <- c(y.i, mean(pred[pred >= breaks[i] & pred < breaks[i + 1]]))
-  }
-
-  y.i <- y.i[minimumIndex]
+  ## FIXME: (ML) Do we want to use `brier()`
+  ## * Returns returns midpoints of intervals
+  ## * Also removes NAs in case no observations are within breaks
+  minimumIndex <- which(temp$prob.y * N >= minimum)
+  y.i <- temp$y.i[minimumIndex]  
   obar.i <- temp$obar.i[minimumIndex]
-  obar.i[is.na(obar.i)] <- 0  ## FIXME: (ML) Include by me as I got NAs, how could that happen? miminimIndex takes care of that! 
- 
-  ## FIXME: (ML) My version to not use `brier()` - works only with `bins = TRUE`
+
+  ## Jakob's style for y.i
+  #y.i <- NULL
+  #for (i in 1:(length(breaks) - 1)) {
+  #  y.i <- c(y.i, mean(pred[pred >= breaks[i] & pred < breaks[i + 1]]))
+  #}
+  ## Moritz' style for y.i -> problem here, when no predictions within intervals
+  #y.i <- as.numeric(aggregate(pred, by = list(cut(pred, breaks, include.lowest = TRUE)), mean)[,2])
+
+  ## Moritz' style for y.i
   #obar.i <- as.numeric(
   #  aggregate(obs, by = list(cut(pred, breaks, include.lowest = TRUE)), mean)[, 2]
   #)
-  #y.i <- na.omit(as.numeric(
-  #  filter(breaks, filter = c(0.5, 0.5))
-  #))
 
-  ### FIXME: (ML) Included by Reto, what is it for?
-  #meanpred.i <- as.numeric(
-  #  aggregate(pred, by = list(cut(pred, breaks, include.lowest = TRUE)), mean, drop = FALSE)[, 2]
-  #)
-  #meanpred.i[is.na(meanpred.i)] <- 0 ## FIXME: (ML) I got NAs, why can that happen?
 
   ## consistency resampling from Broecker (2007) 
   obar.i.boot <- NULL
@@ -100,10 +93,10 @@ reliagram.default <- function(object,
     ## surrogate observations that are reliable by construction
     yhat <- runif(N) < xhat
 
-    ### compute observed relative frequencies of the surrogate observations ## can lead to Error
+    ### compute observed relative frequencies of the surrogate observations
     temp <- brier(obs = yhat, pred = xhat, thresholds = breaks, bins = bins)
     temp_obar.i <- temp$obar.i
-    temp_obar.i[is.na(temp_obar.i)] <- 0  ## FIXME: (ML) Include by me as I got NAs, how could that happen? 
+    temp_obar.i[is.na(temp_obar.i)] <- 0  # FIXME: (ML) Included as I got NAs (see above) 
     obar.i.boot <- rbind(obar.i.boot, temp_obar.i)
   }
 
@@ -111,26 +104,21 @@ reliagram.default <- function(object,
   lowerlimit <- apply(obar.i.boot, 2, quantile, prob = prob[1], na.rm = TRUE)[minimumIndex]
   upperlimit <- apply(obar.i.boot, 2, quantile, prob = prob[2], na.rm = TRUE)[minimumIndex]
 
-  ## auxiliary variable for polygon function (shading)
-  shadedarea <- na.omit(cbind(c(y.i, rev(y.i)), c(upperlimit, rev(lowerlimit))))
-
   ## return value
-  rval <- list(
-    diagram = data.frame(
-      obar.i, 
-      y.i, 
-      lowerlimit, 
-      upperlimit
-    ), 
-    shading = shadedarea, 
-    bs = mean((obs - pred)^2), 
-    prob.y = N * temp$prob.y
+  rval <- data.frame(
+    obar.i, 
+    y.i, 
+    lowerlimit, 
+    upperlimit
+    ## FIXME: (ML) Still needed? If yes subset with minimumIndex and use not temp of resampling
+    #prob.y = N * temp$prob.y  
   )
 
   ## attributes for graphical display
   attr(rval, "xlab") <- xlab
   attr(rval, "ylab") <- ylab
-  class(rval) <- c("reliagram", "list")  ## FIXME: (ML) Change to data.frame
+  attr(rval, "bs") <- mean(obs - pred)^2
+  class(rval) <- c("reliagram", "data.frame")
 
   ## plot by default
   if (plot) {
@@ -155,24 +143,41 @@ plot.reliagram <- function(object,
                            type = "b", 
                            print.bs = FALSE, 
                            col = "black", 
+                           fill = "lightgray",
+                           extend = TRUE, 
                            ...) {
 
-  plot(0, 0, type = "n", xlim = xlim, ylim = ylim, xlab = xlab, ylab = ylab, lwd = lwd, col = col, ...)
+  ## auxiliary variable for polygon function (shading)
+  shading <- na.omit(cbind(c(object$y.i, rev(object$y.i)), c(object$upperlimit, rev(object$lowerlimit))))
+
+  plot(0, 0, type = "n", xlim = xlim, ylim = ylim, xlab = xlab, ylab = ylab, lwd = lwd, col = col, 
+    xaxs = "i", yaxs = "i", ...)
 
   if(!identical(shaded, FALSE)) {
     if(identical(shaded, TRUE)) shaded <- rgb(col2rgb(col)[1]/255, col2rgb(col)[2]/255, col2rgb(col)[3]/255, alpha = 0.1)
     with(object, polygon(shading[,1], shading[,2], col = shaded, border = NA))
   }
 
-  if(!identical(confint, FALSE)) {
-    if(identical(confint, TRUE)) confint <- "darkgray"
-    lines(lowerlimit ~ y.i, data = object$diagram, col = confint)
-    lines(upperlimit ~ y.i, data = object$diagram, col = confint)
+  if(confint) {
+    if(extend) { 
+      polygon(
+        c(0, object$y.i, 1, rev(object$y.i), 0), 
+        c(0, object$lowerlimit, 1, rev(object$upperlimit), 0), 
+        col = fill, border = fill
+      )
+    } else {
+      polygon(
+        c(object$y.i, rev(object$y.i)), 
+        c(object$lowerlimit, rev(object$upperlimit)), 
+        col = fill, border = fill
+      )
+    }
+    box()
   }
 
-  if(abline) abline(0, 1, lty = 3)
-  lines(obar.i ~ y.i, object$diagram, type = type, lwd = lwd, col = col, ...)
-  if(print.bs) text(0,1, paste0("BS = ", signif(object$bs, 4)), adj = c(0,1))
+  if(abline) abline(0, 1, lty = 2)
+  lines(obar.i ~ y.i, object, type = type, lwd = lwd, col = col, ...)
+  if(print.bs) text(0,1, paste0("BS = ", signif(attr(object, "bs"), 4)), adj = c(0,1))
 }
 
 
