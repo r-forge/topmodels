43a168ffb6afa87c1b25e7fb4f964d8613853af0
mlang (Moritz Lang) <moritz.n.lang@gmail.com>
2021-04-25 11:29:58 +0000

    Inlcude sanity checks and make sure single_graph works properly
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/topmodels@1233 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/topmodels/R/reliagram.R
+++ pkg/topmodels/R/reliagram.R
@@ -28,30 +28,34 @@ reliagram.default <- function(object,
                               confint = TRUE, 
                               confint_level = 0.95,
                               confint_nboot = 250, 
+                              single_graph = FALSE,
                               xlab = "Forecast probability", 
                               ylab = "Observed relative frequency",
                               main = NULL,
                               ...) {
 
-  ## sanity checks (breaks is checked within `cut()`)
+  ## sanity checks 
+  ## (`object` and `newdata` w/i `newrepsone()`, `breaks w/i `cut()`, `...` w/i `plot()`)
   stopifnot(is.logical(plot))
-  stopifnot(is.numeric(probs) && is.null(dim(probs)))
+  stopifnot(is.numeric(probs), is.null(dim(probs)))
   stopifnot(is.null(thresholds) || (is.numeric(thresholds) && is.null(dim(thresholds))))
   stopifnot(is.null(y) || (is.numeric(y) && is.null(dim(y))))
   stopifnot(is.logical(confint))
   stopifnot(
-    is.numeric(confint_level) &&
-    is.null(dim(confint_level)) &&
-    length(confint_level) == 1 &&
-    confint_level >= 0 &&
+    is.numeric(confint_level),
+    length(confint_level) == 1,
+    confint_level >= 0,
     confint_level <= 1
   )
   stopifnot(
-    is.numeric(confint_nboot) &&
-    is.null(dim(confint_nboot)) &&
-    length(confint_level) == 1 &&
+    is.numeric(confint_nboot),
+    length(confint_level) == 1,
     confint_level >= 0
   )
+  stopifnot(is.logical(single_graph))
+  stopifnot(length(xlab) == 1 || length(xlab) == length(probs))
+  stopifnot(length(ylab) == 1 || length(ylab) == length(probs))
+  stopifnot(is.null(main) || (length(main) == 1 || length(main) == length(probs)))
 
   ## fix length of annotations
   if(length(xlab) < length(probs)) xlab <- rep(xlab, length.out = length(probs))
@@ -63,17 +67,13 @@ reliagram.default <- function(object,
 
   ## data + thresholds
   y <- if(is.null(y)) newresponse(object, newdata = newdata) else as.numeric(y)
-  ## FIXME: (ML) This can lead to an error
-  ## R> x <- 1
-  ## R> x <- if(is.null(x)) 2
-  ## R> x
-  ## NULL 
-  thresholds <- if(is.null(thresholds)) quantile(y, probs = probs, na.rm = TRUE) 
+  thresholds <- if(is.null(thresholds)) quantile(y, probs = probs, na.rm = TRUE) else thresholds
   thresholds <- as.numeric(thresholds)
 
   ## predicted probabilities
   pred <- procast(object, newdata = newdata, type = "probability", at = matrix(thresholds, nrow = 1L),
     drop = FALSE)
+  stopifnot(NROW(pred) == length(y))  # FIXME: (ML) allow argument `y` or only allow argument `newdata`
 
   ## get and prepare observations
   y <- sapply(thresholds, function(x) y <= x)
@@ -172,7 +172,7 @@ reliagram.default <- function(object,
 
   ## plot by default
   if (plot) {
-    plot(rval, confint = confint, ...)
+    try(plot(rval, confint = confint, single_graph = single_graph, ...))
   }
 
   ## return invisibly
@@ -182,90 +182,149 @@ reliagram.default <- function(object,
 
 ## actual drawing
 plot.reliagram <- function(x, 
-                           minimum = 0,
-                           confint = TRUE, 
-                           abline = TRUE,
-                           xlim = c(0, 1), 
-                           ylim = c(0, 1), 
-                           xlab = NULL,
-                           ylab = NULL,
-                           main = NULL,
-                           col = "black",
-                           fill = adjustcolor(1, alpha.f = 0.2),
-                           lwd = 2, 
-                           pch = 19,
-                           type = "b", 
-                           print_info = FALSE, 
-                           extend_left = NULL, 
-                           extend_right = NULL, 
+                           single_graph = FALSE,  # logical
+                           minimum = 0,  # single or n values
+                           confint = TRUE,  # single or n values 
+                           diag = TRUE,  # single or n values, logical or color
+                           xlim = c(0, 1),  # single vector of lenght 2, or list w/ 2 vectors of length n
+                           ylim = c(0, 1),  # single vector of lenght 2, or list w/ 2 vectors of length n
+                           xlab = NULL,  # single or n values   
+                           ylab = NULL,  # single or n values 
+                           main = NULL,  # single or n values 
+                           col = "black",  # single or n colors 
+                           fill = adjustcolor("black", alpha.f = 0.2),  # single or n colors 
+                           lwd = 2,   # single or n colors 
+                           pch = 19,  # single or n colors 
+                           type = "b",  # single or n colors 
+                           print_info = TRUE,  # single or n colors  
+                           extend_left = NULL, # either null or logical of length 1 / n
+                           extend_right = NULL, # either null or logical of length 1 / n
                            ...) {
+  ## sanity checks 
+  ## (lengths of all arguments are checked by recycling, `diag` w/i abline, `...` w/i `plot()`)
+  stopifnot(is.logical(single_graph))
+  stopifnot(is.numeric(minimum), all(minimum >= 0))
+  stopifnot(is.logical(confint))
+  stopifnot(is.logical(print_info))
+  stopifnot(is.null(extend_left) || is.logical(extend_left))
+  stopifnot(is.null(extend_right) || is.logical(extend_right))
 
   ## handling of groups
   if (is.null(x$group)) x$group <- 1L
   n <- max(x$group)
 
+  ## recycle arguments for plotting to match the number of groups
+  if (is.null(extend_left)) extend_left <- NA
+  if (is.null(extend_right)) extend_right <- NA
+  plot_arg <- data.frame(1:n, minimum, confint, diag, xlim1 = xlim[[1]], xlim2 = xlim[[2]], 
+    ylim1 = ylim[[1]], ylim2 = ylim[[2]], col, fill, lwd, pch, type,
+    print_info, extend_left, extend_right)[, -1]
+
   ## annotation
-  if (is.null(xlab)) xlab <- TRUE
-  if (is.null(ylab)) ylab <- TRUE
-  if (is.null(main)) main <- TRUE
-  xlab <- rep(xlab, length.out = n)
-  ylab <- rep(ylab, length.out = n)
-  main <- rep(main, length.out = n)
-  if (is.logical(xlab)) xlab <- ifelse(xlab, attr(x, "xlab"), "")
-  if (is.logical(ylab)) ylab <- ifelse(ylab, attr(x, "ylab"), "")
-  if (is.logical(main)) main <- ifelse(main, attr(x, "main"), "")
+  if (single_graph) {
+    if (is.null(xlab)) xlab = "Forecast probability"
+    if (is.null(ylab)) ylab = "Observed relative frequency"
+    if (is.null(main)) main <- "Reliability Diagram"
+  } else {
+    if (is.null(xlab)) xlab <- TRUE
+    if (is.null(ylab)) ylab <- TRUE
+    if (is.null(main)) main <- TRUE
+    xlab <- rep(xlab, length.out = n)
+    ylab <- rep(ylab, length.out = n)
+    main <- rep(main, length.out = n)
+    if (is.logical(xlab)) xlab <- ifelse(xlab, attr(x, "xlab"), "")
+    if (is.logical(ylab)) ylab <- ifelse(ylab, attr(x, "ylab"), "")
+    if (is.logical(main)) main <- ifelse(main, attr(x, "main"), "")
+  }
 
   ## plotting function
   reliagramplot1 <- function(d, ...)  {
 
-    ## get lines with sufficient observations
-    min_idx <- which(d$n_pred >= minimum)
-
-    ## check where ci should be extended
-    if(is.null(extend_left)) extend_left <- 1 %in% min_idx
-    if(is.null(extend_right)) extend_right <- nrow(d) %in% min_idx
-
     ## get group index
     j <- unique(d$group)
 
+    ## get lines with sufficient observations
+    min_idx <- which(d$n_pred >= plot_arg$minimum[j])
+
+    ## check where ci should be extended
+    if (is.na(plot_arg$extend_left[j])) plot_arg$extend_left[j] <- 1 %in% min_idx
+    if (is.na(plot_arg$extend_right[j])) plot_arg$extend_right[j] <- nrow(d) %in% min_idx
+
     ## trigger plot
-    plot(0, 0, type = "n", xlim = xlim, ylim = ylim, xlab = xlab[j], ylab = ylab[j], main = main[j], 
-      xaxs = "i", yaxs = "i", ...)
-    box()
-  
+    if (j == 1) {
+      plot(0, 0, type = "n", xlim = c(plot_arg$xlim1[j], plot_arg$xlim2[j]), 
+        ylim = c(plot_arg$ylim1[j], plot_arg$ylim2[j]), xlab = xlab[j], 
+        ylab = ylab[j], main = main[j], 
+        xaxs = "i", yaxs = "i", ...)
+      box()
+    } else if (!single_graph && j > 1) {
+      plot(0, 0, type = "n", xlim = c(plot_arg$xlim1[j], plot_arg$xlim2[j]), 
+        ylim = c(plot_arg$ylim1[j], plot_arg$ylim2[j]), xlab = xlab[j], 
+        ylab = ylab[j], main = main[j], 
+        xaxs = "i", yaxs = "i", ...)
+      box()
+    }
+
     ## plot ci
-    if (confint & !is.na(attr(d, "confint_level"))) {
+    if (plot_arg$confint[j] & !is.na(attr(d, "confint_level"))) {
       polygon(
         na.omit(c(
-          ifelse(extend_left, 0, NA), 
+          ifelse(plot_arg$extend_left[j], 0, NA), 
           d[min_idx, "mean_pr"], 
-          ifelse(extend_right, 1, NA), 
+          ifelse(plot_arg$extend_right[j], 1, NA), 
           rev(d[min_idx, "mean_pr"]), 
-          ifelse(extend_left, 0, NA)
+          ifelse(plot_arg$extend_left[j], 0, NA)
         )), 
         na.omit(c(
-          ifelse(extend_left, 0, NA), 
+          ifelse(plot_arg$extend_left[j], 0, NA), 
           d[min_idx, "ci_lwr"], 
-          ifelse(extend_right, 1, NA), 
+          ifelse(plot_arg$extend_right[j], 1, NA), 
           rev(d[min_idx, "ci_upr"]), 
-          ifelse(extend_left, 0, NA)
+          ifelse(plot_arg$extend_left[j], 0, NA)
         )), 
-        col = fill, border = NA
+        col = plot_arg$fill[j], border = NA
       )
     }
 
-    ## plot perfect prediction
-    if(abline) abline(0, 1, lty = 2)
+    ## plot reference diagonal
+    if (j == 1) {
+      if(!identical(plot_arg$diag[j], FALSE)) {
+        if(isTRUE(plot_arg$diag[j])) plot_arg$diag[j] <- "black"
+        abline(0, 1, col = plot_arg$diag[j], lty = 2)
+      }
+    } else if (!single_graph && j > 1) {
+      if(!identical(plot_arg$diag[j], FALSE)) {
+        if(isTRUE(plot_arg$diag[j])) plot_arg$diag[j] <- "black"
+        abline(0, 1, col = plot_arg$diag[j], lty = 2)
+      }
+    }
 
     ## plot reliability line
-    lines(obs_rf ~ mean_pr, d[min_idx, ], type = type, lwd = lwd, pch = pch, col = col, ...)
+    lines(obs_rf ~ mean_pr, d[min_idx, ], type = plot_arg$type[j], lwd = plot_arg$lwd[j], 
+      pch = plot_arg$pch[j], col = plot_arg$col[j], ...)
 
     ## print info
-    if(print_info) text(0, 1, paste0("BS = ", signif(attr(x, "bs"), 4)), adj = c(0,1))
+    if(single_graph && j == 1 && plot_arg$print_info[j]) {
+      legend(
+        "bottomright", 
+        sprintf("%s = %.4f", attr(d, "main"), signif(attr(x, "bs"), 4)),
+        pch = plot_arg$pch,
+        col = plot_arg$col,
+        bty = "n",
+        title = "Brier Score",
+        y.intersp = 0.9
+      )
+    } else if (!single_graph && plot_arg$print_info[j]) {
+      legend(
+        "bottomright", 
+        sprintf("BS = %.4f", signif(attr(d, "bs")[j], 4)),
+        bty = "n"
+      )
+    }
   }
 
   ## draw plots
-  if (n > 1L) par(mfrow = n2mfrow(n))
+  if (!single_graph && n > 1L) par(mfrow = n2mfrow(n))
   for (i in 1L:n) reliagramplot1(x[x$group == i, ], ...)
 }
 
