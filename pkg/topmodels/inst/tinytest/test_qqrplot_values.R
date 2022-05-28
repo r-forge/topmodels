767f30da61fc06bf0ce91d99873880e9a849b631
retos (Reto Stauffer) <reto.stauffer@uibk.ac.at>
2022-05-28 09:42:58 +0000

    feat(test_qqrplot_values.R): Added some additional tests, parts may be missing but also checking the entire object structure; not sure if OS independent!
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/topmodels@1607 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/topmodels/inst/tinytest/test_qqrplot_values.R
+++ pkg/topmodels/inst/tinytest/test_qqrplot_values.R
@@ -4,6 +4,7 @@
 
 if (interactive()) { rm(list = objects()); library("devtools"); library("tinytest"); library("topmodels") }
 
+library("digest")
 library("crch")
 library("ggplot2")
 library("tibble")
@@ -60,6 +61,30 @@ expect_identical(NROW(CrabSatellites), NROW(q3n), info = "testing dimension 1 (n
 
 
 # --------------------------------------------------------------------
+# As we are using a fixed seed; check md5 sum of the returned objects.
+# In case a test fails it is not precise but shows that there are differences.
+# TODO: (RS2ML) Not sure if this works across different OSs!!!
+# --------------------------------------------------------------------
+expect_identical(digest(q1u), "641a9d725b724d97757c7a1b83ca6883")
+expect_identical(digest(q1n), "950d8cf9f6064b7368698fd44c69fbb6")
+expect_identical(digest(q2u), "9429373875f9dbb8286a4d9308d4ef0a")
+expect_identical(digest(q2n), "4eb11af5b60908c41a0f7b00c6de6df9")
+expect_identical(digest(q3u), "89a51471444630b56275d60dc9db5d5a")
+expect_identical(digest(q3n), "69ee4fb089738de8a03899c47922f063")
+
+# Thus, altenratively, gong for colSums(abs()) for all finite values
+# which is a much weaker test.
+fn <- function(x) as.numeric(sapply(x, function(y) ifelse(all(is.na(y)), NA, sum(abs(y[is.finite(y)])))))
+expect_equal(fn(q1u), c(23.8823086240537, 25, NA, NA, NA))
+expect_equal(fn(q1n), c(38.4240155605576, 39.6761730673991, NA, NA, NA))
+expect_equal(fn(q2u), c(23.6139646074072, 25, NA, NA, NA))
+expect_equal(fn(q2n), c(38.5067890113182, 39.6761730673991, NA, NA, NA))
+expect_equal(fn(q3u), c(74.4564233315605, 86.5, 72.0636565265156, 78.8877997788859, 86.5))
+expect_equal(fn(q3n), c(241.192716338567, 137.831905004867, 251.110835542692, 239.737769513666, 137.831905004867))
+rm(fn)
+
+
+# --------------------------------------------------------------------
 # Per definition, uniform quantiles (observed and expected) must
 # be within [0, 1].
 # --------------------------------------------------------------------
@@ -69,58 +94,55 @@ expect_true(all(sapply(list(q1u, q2u, q3u), function(x) all(x$observed >= 0 & x$
 
 # LINEAR MODEL ----------------------------
 # Calculate residuals with ML corrected standard deviation (scale)
-eps   <- residuals(m1)
-##delta <- min(diff(sort(unique(model.response(model.frame(m1)))))) / 5e6
-scale <- sqrt(1 / length(eps) * sum((eps - mean(eps))^2))
-expect_equivalent(q1n$observed, residuals(m1) / scale)
-expect_equivalent(q1u$observed, qunif(pnorm(residuals(m1) / scale)))
+m1_eps   <- residuals(m1)
+m1_scale <- sqrt(1 / length(m1_eps) * sum((m1_eps - mean(m1_eps))^2))
+m1_prob  <- pnorm(model.response(model.frame(m1)),
+                  mean = fitted(m1),
+                  sd   = m1_scale)
+expect_equivalent(q1n$observed, qnorm(m1_prob))
+expect_equivalent(q1u$observed, m1_prob)
 
-# TODO: (RS2ML) This fails as order(order(eps)) is not identical as two
-#       data points are identical and the order is, thus, no longer unique.
+# NOTE: (RS2ML) Comparing ordered qq-(r)-residuals does not work as
+#       observations are not unique, thus the order itself is not unique.
 ##expect_equivalent(q1u$expected, ppoints(length(eps))[order(order(eps))])
 ##plot(q1u$expected, ppoints(length(eps))[order(order(eps))])
+expect_equivalent(sort(q1n$expected), qnorm(ppoints(length(m1_eps))))
+expect_equivalent(sort(q1u$expected), ppoints(length(m1_eps)))
 
-expect_equivalent(sort(q1n$expected), qnorm(ppoints(length(eps))))
-expect_equivalent(sort(q1u$expected), ppoints(length(eps)))
-
-
-## CRCH MODEL ------------------------------
-## Same for the censored model
-#eps   <- residuals(m2, type = "standardized")
-#idx   <- which(y > m2$cens$left)
-#expect_equivalent(q2n$observed[idx], eps[idx])                      # observed [non-censored]
-#expect_equivalent(sort(q2n$expected), qnorm(ppoints(length(eps))))  # expected
-## TODO: (RS2ML) This fails as order(order(eps)) is not identical as two
-##       data points are identical and the order is, thus, no longer unique.
-###expect_equivalent(q2n$expected, qnorm(ppoints(length(eps)))[order(order(eps))])  # expected
-#plot(q2u$expected, ppoints(length(eps))[order(order(eps))])
-#
-## ------> da ist was mit meiner standardized order falsch oder? Ist das nicht einfach der CENS?
-#
-#
-#expect_equivalent(q2n$expected, qnorm(ppoints(length(eps)))[order(order(eps))])  # expected
-#
-#plot(q2n$expected, qcnorm(ppoints(length(eps)), left = m2$cens$left)[order(order(eps))])  # expected
-#k <- data.frame(q = q2n$expected, order = order(order(eps)), reto = qnorm(ppoints(length(eps)))[order(order(eps))])
-#subset(k, order == 1)
-#which(k$q < -2)
-#k[1,] 
-#plot(reto ~ q, data = k, col = ifelse(order == 1, 'red', 'black'))
-#
-#head(q2n)
-#
-#y     <- model.response(model.frame(m2))
-#
-#expect_equivalent(qunif(pnorm(eps)), q2u$observed) # TODO: (RS) Hm ..
-#
-#head(q2n)
-#hist(q2n$expected)
-#f <- sort(q2n$expected)
+rm(m1_eps, m1_scale, m1_prob)
 
 
+# CRCH MODEL ------------------------------
+# Same for the censored model
+m2_eps  <- residuals(m2, type = "standardized")
+m2_prob <- pcnorm(model.response(model.frame(m2)),
+                mean = fitted(m2, "location"),
+                sd   = fitted(m2, "scale"),
+                left = m2$cens$left, right = m2$cens$right)
+expect_equal(q2n$observed, qnorm(m2_prob))
+expect_equal(sort(q2n$expected), qnorm(ppoints(length(m2_eps))))
+
+expect_equal(q2u$observed, m2_prob)
+expect_equal(sort(q2u$expected), qunif(ppoints(length(m2_eps))))
+
+rm(m2_eps, m2_prob)
 
 
+# POISSON MODEL ---------------------------
+m3_eps   <- residuals(m3)
+m3_delta <- min(diff(sort(unique(model.response(model.frame(m3)))))) / 5e6
+m3_prob1 <- ppois(model.response(model.frame(m3)) - m3_delta, lambda = fitted(m3))
+m3_prob2 <- ppois(model.response(model.frame(m3)),            lambda = fitted(m3))
 
+# Oberved and expected are based on randomized quantile reiduals.
+# Observed but must lie in ppois(y - delta | lambda) and ppois(y | lambda).
+expect_true(all(q3n$observed >= qnorm(m3_prob1) & q3n$observed <= qnorm(m3_prob2)))
+expect_true(all(q3u$observed >= m3_prob1        & q3u$observed <= m3_prob2))
+
+# TODO: (RS2ML) How to test the expected randomized quantile residuals?
+#       How to test the simulated confidenc intervals?
+####### m3_exp <- ppoints(length(m3_eps))[order(order(m3_eps))]
+####### expect_true(all(abs(q3u$expected - m3_exp) < q3u$simint_expected))
 
 
 
