6665c1e8eacb75d166c6fb45a762ede66f8e7411
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2023-01-07 02:21:10 +0000

    drop tests with disttree as well
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/topmodels@1701 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/topmodels/inst/tinytest/test_topmodels_functions.R
+++ pkg/topmodels/inst/tinytest/test_topmodels_functions.R
@@ -14,8 +14,6 @@ data("CrabSatellites", package = "countreg")
 m1 <- lm(dist ~ speed, data = cars)
 m2 <- crch(dist ~ speed | speed, left = 3, data = cars)
 m3 <- glm(satellites ~ width + color, data = CrabSatellites, family = poisson)
-m4 <- disttree(dist ~ speed, data = cars)
-m5 <- distforest(dist ~ speed, data = cars)
 
 # FIXME: (ML) These all fail as for `do.call()` w/i `topmodels()` model object is not found
 expect_silent( 
@@ -34,12 +32,3 @@ expect_warning(
 expect_silent( 
   topmodels(m3, plot = "ggplot2", colour = "black", nsim = 10L)
 )
-
-expect_silent( 
-  topmodels(m4, plot = "base")
-)
-
-expect_silent( 
-  topmodels(m5, plot = "ggplot2", colour = "black", nsim = 10L)
-)
-
