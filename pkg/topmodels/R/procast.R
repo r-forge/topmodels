2ad7846e3b041d7c9d5759920d9c4997beb61540
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2023-11-04 20:54:00 +0000

    avoid disstree link in docs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/topmodels@1754 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/topmodels/R/procast.R
+++ pkg/topmodels/R/procast.R
@@ -242,7 +242,7 @@ procast.bamlss <- function(object, newdata = NULL, na.action = na.pass, type = "
 #' @param distributions3 logical. If a dedicated \pkg{distributions3} object
 #' is available (e.g., such as \code{\link[distributions3]{Normal}}) and uses
 #' the same parameterization, should this be used instead of the general
-#' \code{\link[disttree]{DISTTREE}} distribution?
+#' \code{disttree} distribution?
 procast.disttree <- function(object, newdata = NULL, na.action = na.pass, type = "distribution", at = 0.5, drop = FALSE, ..., distributions3 = FALSE) {
   object <- if(is.null(newdata)) {
     distributions3::prodist(object, distributions3 = distributions3)
