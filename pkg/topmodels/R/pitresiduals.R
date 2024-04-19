317f6fa31057a6548e3ef6f5643c38adffd5f92e
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2024-04-19 01:02:41 +0000

    fix bug in newresponse call
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/topmodels@1772 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/topmodels/R/pitresiduals.R
+++ pkg/topmodels/R/pitresiduals.R
@@ -125,7 +125,7 @@ pitresiduals.default <- function(object,
     ## FIXME: (ML) Implement correct NA handling: 
     ## * In newresponse() for y
     ## * Which is used for at w/i procast()
-    y <- newresponse(object, newdata = newdata, na.action = na.action)
+    y <- newresponse(object, newdata = newdata, na.action = na.pass)
     y[["(weights)"]] <- NULL
     if (ncol(y) > 1L) stop("multivariate responses not supported yet")
     y <- y[[1L]]
